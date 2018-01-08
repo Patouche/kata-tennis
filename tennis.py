18989848fcc29d343d7a5c1c5b9e7309cc761154
Patrick Allain <patrick.allain@soat.fr>
2018-01-08 09:46:04 +0100

    US-1 : Add score evolution test

--- tennis.py
+++ tennis.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 
+SCORES = ('0', '15', '30', '40', 'WIN GAME')
+
 
 class Tennis(object):
+    """Tennis game."""
+
     def __init__(self):
+        """Class constructor."""
         self.game = (0, 0)
 
     def score(self):
         return ':'.join(tuple(map(str, self.game)))
+
+    def _win_point(self, param):
+        pass
+
+    def player1_win_point(self):
+        """Method to invoke when the player 1 win the point."""
+        self._win_point((1, 0))
+
+    def player2_win_point(self):
+        """Method to invoke when the player 1 win the point."""
+        self._win_point((0, 1))
--- test_tennis.py
+++ test_tennis.py
@@ -6,11 +6,31 @@ from tennis import Tennis
 
 class TestTennis(unittest.TestCase):
     def test_game_starts_with_a_score_of_0_point_for_each_player(self):
-        game = Tennis()
+        tennis = Tennis()
 
-        score = game.score()
+        score = tennis.score()
 
         self.assertEqual(score, '0:0')
+
+    def test_players_score_progress(self):
+        tennis = Tennis()
+        tennis.player1_win_point()
+        tennis.player2_win_point()
+
+        self.assertEqual(tennis.score(), '15:15')
+
+        tennis.player1_win_point()
+        tennis.player2_win_point()
+
+        self.assertEqual(tennis.score(), '30:30')
+
+        tennis.player1_win_point()
+
+        self.assertEqual(tennis.score(), '40:30')
+
+        tennis.player1_win_point()
+
+        self.assertEqual(tennis.score(), 'WIN:30')
 
 
 if __name__ == '__main__':
